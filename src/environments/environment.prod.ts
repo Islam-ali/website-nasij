984c89c83f94167df5a138b8413086b149a89990
islam-ali <ai4625511@gmail.com>
2025-09-09 07:10:09 +0300

    Update production environment configuration to point to new backend domain and API URL for deployment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  domain: 'https://pledge-backend-sigma.vercel.app',
-  apiUrl: 'https://pledge-backend-sigma.vercel.app/api/v1', // Development API URL
+  domain: 'https://pledge-backend-roan.vercel.app/',
+  apiUrl: 'https://pledge-backend-roan.vercel.app//api/v1', // Development API URL
   appName: 'Online Store (Dev)',
   appVersion: '1.0.0',
   stripePublishableKey: 'your-test-stripe-key',
